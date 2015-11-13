d0afc4c110d853507a0b29f840987341cbb49a62
ddutra9 <ddutra9@gmail.com>
2015-11-12 22:30:46 -0200

    conclued create a image file by app mobile

--- src/main/java/com/tcc/printmobile_web/service/Print.java
+++ src/main/java/com/tcc/printmobile_web/service/Print.java
@@ -1,10 +1,13 @@
 package com.tcc.printmobile_web.service;
 
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.imageio.ImageIO;
 import javax.print.DocFlavor;
 import javax.print.DocPrintJob;
 import javax.print.PrintException;
@@ -22,6 +25,7 @@ import javax.print.attribute.standard.PageRanges;
 
 import com.sun.jersey.core.util.Base64;
 import com.tcc.printmobile_web.model.File;
+import com.tcc.printmobile_web.model.Img;
 import com.tcc.printmobile_web.model.Pdf;
 
 public class Print {
@@ -32,8 +36,22 @@ public class Print {
 		if (file instanceof Pdf)
 			pdf = (Pdf) file;
 
-		InputStream prin = new ByteArrayInputStream(Base64.decode(file
-				.getByteBase64()));
+		byte[] bytearray = Base64.decode(file.getByteBase64());
+
+		InputStream prin = null;
+
+		if (file instanceof Img) {
+			BufferedImage imag = ImageIO.read(new ByteArrayInputStream(
+					bytearray));
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			// ImageIO.write(imag, "jpg", new java.io.File("/home/ddutra9",
+			// "snap.jpg"));escreve a imagem
+			ImageIO.write(imag, "jpg", baos);
+
+			prin = new ByteArrayInputStream(baos.toByteArray());
+		} else
+			prin = new ByteArrayInputStream(bytearray);
+
 		DocFlavor docFlavor = DocFlavor.INPUT_STREAM.AUTOSENSE;
 		SimpleDoc documentoTexto = new SimpleDoc(prin, docFlavor, null);
 		PrintService impressora = PrintServiceLookup
